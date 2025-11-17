5461d12f95578ac6304fb4d439dddf236356aa3f
sachinbarethtc <sachin.bareth@turningcloud.com>
2025-11-17 17:43:38 +0530

    ui initialized

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const chatbot = document.getElementById("chatbot");
+document.getElementById("openChat").onclick = () => chatbot.classList.add("open");
+document.getElementById("closeChat").onclick = () => chatbot.classList.remove("open");
